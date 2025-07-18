9347052d8a0392697a0a8214448a273273b27fa9
MinamizonoEno <minamizono.e@gmail.com>
2025-07-18 11:32:13 +0900

    feat: add basePath and assetPrefix configurations to next.config.js

--- next.config.js
+++ next.config.js
@@ -2,6 +2,9 @@
 const nextConfig = {
   reactStrictMode: true,
   output: 'export',
+  basePath: isProd ? `/${repo}` : '',
+  assetPrefix: isProd ? `/${repo}/` : '',
+  images: { unoptimized: true },
 }
 
 module.exports = nextConfig
