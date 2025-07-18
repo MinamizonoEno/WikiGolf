7ed84dd2c64260a2542a169e6c4ee2be0421fe87
MinamizonoEno <minamizono.e@gmail.com>
2025-07-18 11:51:21 +0900

    feat: add generator configuration for static site generation in workflow

--- next.config.js
+++ next.config.js
@@ -1,13 +1,8 @@
 /** @type {import('next').NextConfig} */
 
-const isProd = process.env.NODE_ENV === 'production'
-const repo = 'WikiGolf'
-
 const nextConfig = {
   reactStrictMode: true,
   output: 'export',
-  basePath: isProd ? `/${repo}` : '',
-  assetPrefix: isProd ? `/${repo}/` : '',
   images: { unoptimized: true },
 }
 
